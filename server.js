794cb98fb400d1499925d1cccf1cde88154c9f1c
Paul M Herman <pmherman@Pauls-MacBook-Pro-15.local>
2018-02-15 21:01:42 -0500

    no message

--- server.js
+++ server.js
@@ -1,41 +1,37 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var shortid = require("shortid");
 var path = require("path");
+
 var app = express();
+
 var PORT = process.env.PORT || 3000;
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-var reservation = [
-    {
-        routeName: "reservation1",
-        name: "guest1",
-        phoneNumber: "555-555-5555",
-        email: "email@domain.com",
-        uniqueID: "001"
-    }
-];
-var waitList = [
-    {
-        routeName: "waiter1",
-        name: "guest1",
-        phoneNumber: "555-555-5555",
-        email: "email@domain.com",
-        uniqueID: "001"
-    }
-]
+
+var reservation = [{
+    uniqueID: (shortid.generate())
+}];
+var waiter = [];
+
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
-app.get("/tables", function(req, res) {
+
+app.get("/tables.html", function(req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));  
 });
-app.get("/reserve", function (req, res) {
+
+app.get("/reserve.html", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
+
 app.get("/all", function(req, res) {
     res.json(reservation);
   });
-app.get("/api/:reservation?", function(req, res) {
+
+app.get("/reservations/:reservation?", function(req, res) {
     var chosen = req.params.reservation;
   
     if (chosen) {
@@ -43,21 +39,45 @@ app.get("/api/:reservation?", function(req, res) {
       for (var i = 0; i < reservation.length; i++) {
         if (chosen === reservation[i].routeName) {
           return res.json(reservation[i]);
-        } 
+        }
       }
       return res.json(false);
     }
     return res.json(reservation);
   });
+
+  app.get("/waitlist/:waiter?", function(req, res) {
+    var chosen = req.params.waiter;
+  
+    if (chosen) {
+      console.log(chosen);
+      for (var i = 0; i < waiter.length; i++) {
+        if (chosen === waiter[i].routeName) {
+          return res.json(waiter[i]);
+        }
+      }
+      return res.json(false);
+    }
+    return res.json(waiter);
+  });
+
 app.post("/api/new", function(req, res) {
         var newReservation = req.body;
-        newReservation.routeName = newReservation.routeName.replace(/\s+/g, "").toLowerCase();
+        newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
         console.log(newReservation);
-        
-        reservation.push(newReservation);
-        res.json(newReservation);
-        console.log("Reservation");
+        if (reservation.length < 6) {
+            console.log(reservation.length);
+            reservation.push(newReservation);
+            res.json("Reservation: " + newReservation);
+        } else {
+            waiter.push(newReservation);
+            res.json(newReservation);
+            console.log("Wait List: " + waiter.length);
+        };
+
   });
+
   app.listen(PORT, function() {
     console.log("App is listening on PORT " + PORT);
 })
+
